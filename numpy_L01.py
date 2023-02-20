87a7c01c39da8d7c618e1eabd185652631e1b445
BulkaKrz <bulka.krzysztof.78@gmail.com>
2023-02-20 21:49:57 +0100

    examples using random variable statistics, adding rows and columns

--- numpy_L01.py
+++ numpy_L01.py
@@ -109,5 +109,129 @@ arr[:, :-1]
 #we mix the values 
 np.random.shuffle(arr)
 arr
+
+# create and reshape an array 
+X = np.arange(1,26).reshape(5,5)
+X
  
+Ones = np.ones(X.shape)
+Ones
+# multiply matrices
+np.dot(X, Ones)
+diag = np.zeros(X.shape)
+np.fill_diagonal(diag, 1)
+diag
+np.dot(X, diag)
+ 
+# where 
+np.where(X > 10, 1, 0)
+ 
+np.where(X % 2 == 0, 1, 0)
+ 
+np.where(X % 2 == 0, X, 0)
+ 
+np.where(X % 2 == 0, X, X + 1)
+ 
+np.where(X > 10, 2 * X, 0)
+ 
+X_bis = np.where(X > 10, 2*X, 0)
+X_bis
+
+# Counts the number of non-zero values in the array
+np.count_nonzero(X_bis)
+ 
+x = np.array([[10,20,30], [40,50,60]])
+y = np.array([[100], [200]])
+print(np.append(x, y, axis=1))
+ 
+x = np.array([[10,20,30], [40,50,60]])
+y = np.array([[100, 200, 300]])
+print(np.append(x, y, axis=0))
+ 
+#we connect arrays 
+x = np.array([[10,20,30], [40,50,60]])
+print(np.append(x, x, axis=0))
+
+# statistic in numpy
+data = np.array([[11, 4, 8], [3, 2, 1]])
+data
+ 
+np.median(data)
+ 
+np.median(data, axis=0)
+ 
+np.median(data, axis=1)
+ 
+np.mean(data)
+np.mean(data, axis=0)
+np.mean(data, axis=1)
+np.mean(data, dtype=np.float32)
+np.mean(data, dtype=np.float64)
+ 
+np.average(data)
+np.average(data, axis=0)
+np.average(data, axis=1)
+ 
+np.average(data, weights=[0,1,2], axis=1)
+np.average(data, weights=[2,3,4], axis=1)
+
+np.var(data)
+np.var(data, axis=0)
+np.var(data, axis=1)
+
+np.std(data)
+np.std(data, axis=0)
+np.std(data, axis=1)
+
+
+np.mean(data, dtype=np.float32)
+np.mean(data, dtype=np.float64)
+
+# random variables
+rand_a = np.random.rand()
+print(rand_a)
+ 
+rand_a = np.random.rand(2,5)
+print(rand_a)
+ 
+rand_b = np.random.rand(1,5)
+print(rand_b)
+ 
+rand_b = rand_b*100 
+print(rand_b)
+#adding rows
+rand_c = np.vstack((rand_a, rand_b))
+print(rand_c)
+ 
+rand_b = np.random.rand(2,1)
+print(rand_b)
+#adding columns 
+rand_c = np.hstack((rand_a, rand_b))
+print(rand_c)
+ 
+ 
+X = rand_c[:,2]
+print(X)
+ 
+a=3
+b=8
+ 
+y = a * X + b
+print(y)
+ 
+ 
+X = np.random.randint(low=0, high=10, size = 10) 
+print(X)
+X = X *10 
+print(X)
+ 
+np.random.choice(10, size=3, replace=False)
+ 
+np.random.choice(10, size=3, replace=True)
+ 
+idx = np.random.choice(X.shape[0], size=3, replace=False)
+print(X)
+print(idx)
+print(X[idx])
+
 
